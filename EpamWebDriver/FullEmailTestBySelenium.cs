ab9c8e5e075c327556b8c6425eef88fd59349947
scardes93 <scardes93@gmail.com>
2022-11-22 15:21:34 +0600

    True Page Object classes

--- EpamWebDriver/FullEmailTestBySelenium.cs
+++ EpamWebDriver/FullEmailTestBySelenium.cs
@@ -1,4 +1,5 @@
-﻿using NUnit.Framework;
+﻿using EpamWebDriver.PageObjects;
+using NUnit.Framework;
 using OpenQA.Selenium;
 using OpenQA.Selenium.Chrome;
 using OpenQA.Selenium.Firefox;
@@ -20,22 +21,22 @@ namespace ExampleService.Tests
         {
             IWebDriver driver;
 
-            string MailRuUrl = "https://account.mail.ru/";
-            string username = "epamtestmail93@mail.ru";
-            string emailPassword = "EpamTest185";
+
+            // Mail.ru Autorization data
+            
 
             //Page objects For MailRUAutorization() and [Test, Order(2)]; [Test, Order(3)]
-            By NextButton = By.CssSelector("[data-test-id=next-button]");
-            By SubmitButton = By.CssSelector("[data-test-id=submit-button]");
-            By PopUpEmptyError = By.CssSelector("[data-test-id=required]");
-            By PopUpInputError = By.CssSelector("[data-test-id=password-input-error]");
+            //private readonly By NextButton = By.CssSelector("[data-test-id=next-button]");
+            //private readonly By SubmitButton = By.CssSelector("[data-test-id=submit-button]");
+            private readonly By PopUpEmptyError = By.CssSelector("[data-test-id=required]");
+            private readonly By PopUpInputError = By.CssSelector("[data-test-id=password-input-error]");
 
             //Page objects For send email from mail.ru [Test, Order(5)]
-            By WriteLetterButton = By.XPath("//span[@class='compose-button__txt']");
-            By LetterReciverField = By.XPath("//input[@class='container--H9L5q size_s--3_M-_']");
-            By LetterThemeField = By.XPath("//input[@name='Subject']");
-            By LetterContendField = By.XPath("//div[@role='textbox']/div");
-            By SendButton = By.CssSelector("[data-test-id=send]");
+            private readonly By WriteLetterButton = By.XPath("//span[@class='compose-button__txt']");
+            private readonly By LetterReciverField = By.XPath("//input[@class='container--H9L5q size_s--3_M-_']");
+            private readonly By LetterThemeField = By.XPath("//input[@name='Subject']");
+            private readonly By LetterContendField = By.XPath("//div[@role='textbox']/div");
+            private readonly By SendButton = By.CssSelector("[data-test-id=send]");
 
             [OneTimeSetUp]
             public void Setup()
@@ -47,36 +48,13 @@ namespace ExampleService.Tests
                 driver = new ChromeDriver(path + @"\drivers\");
             }
 
-            //Open mail.ru page
-            public void GotoMailRU()
-            {
-                driver.Navigate().GoToUrl(MailRuUrl);
-                driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(10);
-            }
-
-            // Make full autorization on mail.ru
-            public void MailRUAutorization()
-            {
-                // Open mail.ru page
-                GotoMailRU();
-                
-                // Next fill out the sign-up form with email address and a password
-                driver.FindElement(By.Name("username")).SendKeys(username);
-
-                // Go to on page with password
-                driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(10);
-                driver.FindElement(NextButton).Click();
-
-                //enter wrong password and try to enter
-                driver.FindElement(By.Name("password")).SendKeys(emailPassword);
-                driver.FindElement(SubmitButton).Click();
-            }
-
+            
             [Test, Order(1)]
             public void LoadMailInBrowser_ClickSignUp_LoadSuccess()
             {
+                var autorizationMailru = new MailRuAutorizationPageObjects(driver);
+                autorizationMailru.AutorizationInMailRU();
                 // Open mail.ru page
-                GotoMailRU();
 
                 //Check window is loaded
                 Assert.IsTrue(driver.FindElement(By.Id("login-content")).Displayed);
@@ -85,15 +63,10 @@ namespace ExampleService.Tests
             [Test, Order(2)]
             public void Authorization_WithEmptyEmailPassword_AuthorizationError()
             {
+                var autorizationMailru = new MailRuAutorizationPageObjects(driver);
+                
                 // Open mail.ru page again for empty data
-                GotoMailRU();
-
-                // next fill out the sign-up form with email address and try to go in page with password
-                driver.FindElement(By.Name("username")).SendKeys("");
-
-                // try to go in page with password
-                driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(10);
-                driver.FindElement(NextButton).Click();
+                autorizationMailru.AutorizationInMailRU("");
 
                 //Take a error of empty username/email address
                 driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(10);
@@ -104,18 +77,8 @@ namespace ExampleService.Tests
             public void Authorization_WithErrorEmailPassword_AuthorizationError()
             {
                 //  Open mail.ru page again for incorrent data
-                GotoMailRU();
-
-                // next fill out the sign-up form with email address and a password
-                driver.FindElement(By.Name("username")).SendKeys("123");
-
-                // Go to on page with password
-                driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(10);
-                driver.FindElement(NextButton).Click();
-
-                //enter wrong password and try to enter
-                driver.FindElement(By.Name("password")).SendKeys("123");
-                driver.FindElement(SubmitButton).Click();
+                var autorizationMailru = new MailRuAutorizationPageObjects(driver);
+                autorizationMailru.AutorizationInMailRU("123", "123");
 
                 //And catch error of authorization
                 driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(10);
@@ -126,9 +89,12 @@ namespace ExampleService.Tests
             public void Authorization_WithCorrectEmailPassword_AuthorizationSuccess()
             {
                 // Make full autorization on mail.ru
-                MailRUAutorization();
+                var autorizationMailru = new MailRuAutorizationPageObjects(driver);
+                
+                autorizationMailru.AutorizationInMailRU("epamtestmail93@mail.ru", "EpamTest185");
 
                 //And success of authorization
+                driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(30);
                 Assert.AreEqual("Авторизация", driver.Title);
             }
 
@@ -136,8 +102,10 @@ namespace ExampleService.Tests
             public void SendEmail_WithSomeInformation_SendSuccess()
             {
                 // Make full autorization on mail.ru
-                MailRUAutorization();
-                                
+                var autorizationMailru = new MailRuAutorizationPageObjects(driver);
+
+                autorizationMailru.AutorizationInMailRU("epamtestmail93@mail.ru", "EpamTest185");
+
                 // Go to the page with sending email
                 driver.Manage().Window.Maximize();
                 driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(10);
@@ -157,13 +125,13 @@ namespace ExampleService.Tests
 
             // runs once after all tests finished
 
-            [OneTimeTearDown]
-            public void Dispose()
-            {
-                // close down the browser
-                driver.Quit();
-                driver.Dispose();
-            }
+            //[OneTimeTearDown]
+            //public void Dispose()
+            //{
+            //    // close down the browser
+            //    driver.Quit();
+            //    driver.Dispose();
+            //}
         }
     }
 }
---
+++ EpamWebDriver/PageObjects/MailRuAutorizationPageObjects.cs
@@ -0,0 +1,71 @@
+﻿using OpenQA.Selenium;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EpamWebDriver.PageObjects
+{
+    class MailRuAutorizationPageObjects
+    {
+        private IWebDriver driver;
+
+        string MailRuUrl = "https://account.mail.ru/";
+
+        //Page objects For MailRUAutorization 
+        private readonly By NextButton = By.CssSelector("[data-test-id=next-button]");
+        private readonly By SubmitButton = By.CssSelector("[data-test-id=submit-button]");
+        private readonly By UsernameField = By.Name("username");
+        private readonly By PasswordField = By.Name("password");
+
+        private readonly By PopUpEmptyError = By.CssSelector("[data-test-id=required]");
+        private readonly By PopUpInputError = By.CssSelector("[data-test-id=password-input-error]");
+
+        public MailRuAutorizationPageObjects(IWebDriver driver)
+        {
+            this.driver = driver;
+        }
+
+        //Open mail.ru page
+        private void GotoMailRU()
+        {
+            driver.Navigate().GoToUrl(MailRuUrl);
+            driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(10);
+        }
+
+        // Just open window autorization on mail.ru
+        public void AutorizationInMailRU()
+        {
+            GotoMailRU();
+        }
+
+        // Make error login input on mail.ru
+        public void AutorizationInMailRU(string username)
+        {
+            GotoMailRU();
+            //Fill Username(Login) information
+            driver.FindElement(NextButton).Click();
+            driver.FindElement(UsernameField).SendKeys(username);
+
+            // Go to the nest step
+            driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(10);
+            driver.FindElement(NextButton).Click();
+        }
+
+        // Make full autorization on mail.ru
+        public void AutorizationInMailRU(string username, string password) 
+        {
+            GotoMailRU();
+            //Fill Username(Login) information
+            driver.FindElement(NextButton).Click();
+            driver.FindElement(UsernameField).SendKeys(username);
+
+            // Go to the nest step
+            driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(10);
+            driver.FindElement(NextButton).Click();
+
+            //Now fill the password
+            driver.FindElement(PasswordField).SendKeys(password);
+            driver.FindElement(SubmitButton).Click();
+        }
+    }
+}
