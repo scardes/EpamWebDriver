e37a349508a6b9c3c809a7de7a22962983208e76
scardes93 <scardes93@gmail.com>
2022-11-24 11:55:46 +0600

    Add MailRuSetPersonalDataPageObjects

---
+++ EpamWebDriver/PageObjects/MailRuSetPersonalDataPageObjects.cs
@@ -0,0 +1,32 @@
+﻿using NUnit.Framework;
+using OpenQA.Selenium;
+using System;
+
+namespace EpamWebDriver.PageObjects
+{
+    class MailRuSetPersonalDataPageObjects
+    {
+        private IWebDriver driver;
+
+        //Page objects For MailRu Set Personal Data
+        private readonly By NickNameField = By.XPath("//input[@id='nickname']");
+        private readonly By SaveDataButton = By.XPath("//button[@data-test-id='save-button']");
+
+        public MailRuSetPersonalDataPageObjects(IWebDriver driver)
+        {
+            this.driver = driver;
+        }
+
+        public string ReadNickName()
+        {
+            string nickNameContent = driver.FindElement(NickNameField).Text;
+            return nickNameContent;
+        }
+
+        public void SavePersonData()
+        {
+            driver.FindElement(SaveDataButton, 20).Click();
+        }
+
+    }
+}
