184770d25a0be1ef45b8992f057b1aae69dac46e
scardes93 <scardes93@gmail.com>
2022-12-12 12:05:01 +0600

    Add Folder and Xpath

--- EpamWebDriver/PageObjects/MailRuAutorizationPageObjects.cs
+++ EpamWebDriver/PageObjects/MailRuAutorizationPageObjects.cs
@@ -11,10 +11,12 @@ namespace EpamWebDriver.PageObjects
         string MailRuUrl = "https://account.mail.ru/";
 
         //Page objects For MailRUAutorization 
-        private readonly By NextButton = By.CssSelector("[data-test-id=next-button]");
-        private readonly By SubmitButton = By.CssSelector("[data-test-id=submit-button]");
         private readonly By UsernameField = By.Name("username");
         private readonly By PasswordField = By.Name("password");
+        private readonly By NextButton = By.CssSelector("[data-test-id=next-button]");
+        //private readonly By NextButton = By.Xpath("//button[@data-test-id ='next-button']");
+        //private readonly By NextButton = By.Xpath("//span[text()='Ввести пароль']");
+        private readonly By SubmitButton = By.CssSelector("[data-test-id=submit-button]");
         private readonly By PopUpEmptyError = By.CssSelector("[data-test-id=required]");
         private readonly By PopUpInputError = By.CssSelector("[data-test-id=password-input-error]");
 
